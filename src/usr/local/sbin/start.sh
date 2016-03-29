b2e663422792441640e2e5c66196cdd733f83129
Viacheslav Pryimak <vpryimak@intropro.com>
2016-03-29 19:03:07 +0300

    add new option for kibana

--- src/usr/local/sbin/start.sh
+++ src/usr/local/sbin/start.sh
@@ -5,4 +5,4 @@ export ELASTICSEARCH_PORT_9200_TCP_PORT=${ELASTICSEARCH_PORT_9200_TCP_PORT}
 
 sed -i "s/^elasticsearch_url: .*$/elasticsearch_url: \"http:\/\/${ELASTICSEARCH_PORT_9200_TCP_ADDR}:${ELASTICSEARCH_PORT_9200_TCP_PORT}\"/g" /opt/kibana/config/kibana.yml
 
-su kibana -s /bin/bash -c '/opt/kibana/bin/kibana'
+su kibana -s /bin/bash -c '/opt/kibana/bin/kibana -e http://elasticsearch.default.svc.cluster.local:9200'
